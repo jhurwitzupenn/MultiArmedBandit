fffce4a1a67d510d6df686be7c6f723bc68bf844
colehurwitz <farside411@gmail.com>
2017-05-03 00:31:18 -0400

    Implemented Explore and Exploit algorithm

--- strategies/SemiUniformStrategies.py
+++ strategies/SemiUniformStrategies.py
@@ -26,3 +26,28 @@ class EpsilonGreedyStrategy(Strategy):
             sampleMeans[index] = np.divide(cumPayouts[index], cumPlays[index])
 
         return np.sum(payoutPerStep), payoutPerStep
+        
+class ExploreThenExploit(Strategy):
+    """docstring for ExploreThenExploit"""
+    
+    def __init__(self, exploreDepth):
+        self._exploreDepth = exploreDepth
+
+    def run(self, T, bandit):
+        payoutPerStep = np.zeros(T)
+        cumPayouts = np.zeros(bandit.K)
+        cumPlays = np.zeros(bandit.K)
+        sampleMeans = np.zeros(bandit.K)
+        for t in range(T):
+            if t < self._exploreDepth*bandit.K:
+                index = t % bandit.K
+                
+            payout = bandit.getPayout(index)
+            cumPayouts[index] += payout
+            cumPlays[index] += 1
+            payoutPerStep[t] = payout
+            sampleMeans[index] = np.divide(cumPayouts[index], cumPlays[index])
+            if t == self._exploreDepth*bandit.K - 1:
+                index = np.argmax(sampleMeans)
+
+        return np.sum(payoutPerStep), payoutPerStep
