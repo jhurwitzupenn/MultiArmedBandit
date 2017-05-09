Fixed Bug on Softmax

strategies/softMaxStrategies.py
@@ -28,6 +28,7 @@ class SoftMaxStrategyFixed(Strategy):
         divisor = bandit.K
         for t in range(T):
             numerators = np.exp(sampleMeans / self._temperature)
+            divisor = sum(numerators)
             cumProbability = np.divide(numerators, divisor)
 
             p = np.random.uniform(0, 1)
@@ -36,7 +37,6 @@ class SoftMaxStrategyFixed(Strategy):
                 if (p <= 0):
                     index = i
 
-            divisor = sum(numerators)
             plays[t] = index
             payout = bandit.getPayout(index)
             cumPayouts[index] += payout